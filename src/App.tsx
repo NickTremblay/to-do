45d1a8387f06e9745bf38d6ef5ef9a1d69bd0657
NickTremblay <nickt1@uri.edu>
2022-07-15 16:33:11 -0400

    Render TaskList in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { TaskList } from './components/TaskList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>To Do:</h1>
+      <TaskList />
     </div>
   );
 }
