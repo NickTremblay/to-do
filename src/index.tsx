120da29edfaa3807757341f628aad92be1e58be7
NickTremblay <nickt1@uri.edu>
2022-07-22 13:54:40 -0400

    Init Apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import './index.css';
 import App from './App';
+
+const apolloClient = new ApolloClient({
+  uri: 'https://blessed-bear-22.hasura.app/v1/graphql',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
