f26cebec10455254939390e0c9c8fda983223127
NickTremblay <nickt1@uri.edu>
2022-07-22 17:23:01 -0400

    Derive user state & render displayname

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import User from './types/User';
 import './App.css';
 
 function App() {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({
+    id: -1, 
+    displayname: "",
+    username: ""
+  } as User);
 
   const handleSignIn = (u: User) =>{
     setUser(u); 
@@ -16,9 +20,12 @@ function App() {
   // Override main app render with sign in page when no persisted user instance
   if(!window.localStorage.getItem("user")) return <SignIn handleSignIn={handleSignIn} />;
 
+  // Derive user instance from localStorage if it does not exist in state 
+  if(user.id === -1) setUser(JSON.parse(localStorage.getItem("user") || "") as User);
+
   return (
       <div className="App">
-        <h1>To Do:</h1>
+        <h1>{user.displayname}'s To Do:</h1>
         <TaskList />
       </div>
   );
