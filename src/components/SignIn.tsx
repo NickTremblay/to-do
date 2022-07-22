7734ccfdc6dd77556204c97f592d9ae354cbefb3
NickTremblay <nickt1@uri.edu>
2022-07-22 13:30:13 -0400

    Add state management for SignIn form data

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,31 +1,50 @@
-import React from "react";
- 
-const SignIn =  () =>{
+import React, { useState } from "react"; 
+import User from "../types/User";
 
-    const handleSignIn = (e:React.SyntheticEvent) => {
+interface Props{
+    handleSignIn: (u: User) => void; 
+}
+
+const SignIn:React.FC<Props> =  ({handleSignIn}) =>{
+    const [inputs, setInputs] = useState({
+        username:"",
+        displayname: "",
+        newUsername:""
+    });
+
+    const handleChange = (e: React.SyntheticEvent) => {
+        // Cast event target to HTMLInputElement type before reading
+        const name = (e.target as HTMLInputElement).name; 
+        const value = (e.target as HTMLInputElement).value;
+
+        // Update state of form data 
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleSignInSubmit = (e:React.SyntheticEvent) => {
         e.preventDefault();
     }
 
-    const handleSignUp = (e:React.SyntheticEvent) => {
+    const handleSignUpSubmit = (e:React.SyntheticEvent) => {
         e.preventDefault();
     }
 
     return (
         <div className="App SignIn">
-            <form onSubmit={handleSignIn}>
+            <form onSubmit={handleSignInSubmit}>
                 <h2> Sign In</h2>
                 <h4>Username:</h4>
-                <input type="text" />
+                <input type="text" name="username" onChange={handleChange} value={inputs.username} />
                 <button type="submit" className="Button">Go!</button>
             </form>
             <br />
             <hr />
-            <form onSubmit={handleSignUp}>
+            <form onSubmit={handleSignUpSubmit}>
                 <h2> Sign Up</h2>
                 <h4>Displayname:</h4>
-                <input type="text" />
+                <input type="text" name="displayname" value={inputs.displayname} />
                 <h4>Username:</h4>
-                <input type="text" />
+                <input type="text" name="newUsername" value={inputs.newUsername} />
                 <br />
                 <button type="submit" className="Button">Go!</button>
             </form>
