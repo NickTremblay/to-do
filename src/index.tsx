fcd5d3cc95d685135c1fe938535d563f52bf4598
NickTremblay <nickt1@uri.edu>
2022-07-25 14:21:48 -0400

    Provide auth header in all apollo requests

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
 import './index.css';
 import App from './App';
 
-const apolloClient = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'https://blessed-bear-22.hasura.app/v1/graphql',
+});
+
+// Use context to give hasura secret to all apollo requests as auth header
+// Assumes hasura admin secret exists in localStorage to supplement real authentication
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-hasura-admin-secret": localStorage.getItem("secret"),
+    }
+
+  }
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
